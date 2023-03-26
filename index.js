c95b666f4510af3bbae85de30aa7086127949040
Eleftheria Dispiraki <dispirakieleftheria@gmail.com>
2023-03-26 20:32:20 +0100

    added code for creating engineer and intern profile

--- index.js
+++ index.js
@@ -17,35 +17,102 @@ const id = []
 const teamMembers = []
 
 const appMenu = () => {
-    function createManager(){
-        console.log("Build your team!");
+    console.log("Build your team!");
+
+    function buildTeam(){
+
+    }
+
+    function createIntern(){
         inquirer.prompt([
             {
             type: "input",
-            name: "managerName",
-            message: "What's the manager's name?"
+            name: "internName",
+            message: "What is the intern's name?"
             },
             {
                 type: "input",
-                name: "managerId",
-                message: "What's the manager's id?"
+                name: "internId",
+                message: "What is the intern's id?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "Please write the intern's email address!"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                messageL: "What is the intern's school?"
+            }
+        ]).then(answers=>{
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        teamMembers.push(intern);
+        id.push(answers.internId);
+        createTeam();
+        })
+    }
+
+    function createEngineer(){
+        inquirer.prompt([
+            {
+            type: "input",
+            name: "engineerName",
+            message: "What's the engineer's name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What's the engineer's id?"
             },
             {
                 type: "input",
                 name: "managerEmail",
-                message: "Please write the maanger's email address!"
+                message: "Please write the engineer's email address!"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                messageL: "What is the engineer's GitHub username?"
+            }
+        ]).then(answers=>{
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        teamMembers.push(engineer);
+        id.push(answers.engineerId);
+        createTeam();
+        })
+    }
+
+    function createManager(){
+        inquirer.prompt([
+            {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the manager's id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please write the manager's email address!"
             },
             {
                 type: "input",
                 name: "managerOfficeNumber",
-                messageL: "What's the manager's office number?"
+                messageL: "What is the manager's office number?"
             }
         ]).then(answers=>{
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
         teamMembers.push(manager);
         id.push(answers.managerId);
+        createTeam();
         })
     }
+    createEngineer();
     createManager();
 }
 
