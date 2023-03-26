c8511f86c4c33513eb6897f2b2af9cb33a3db863
Eleftheria Dispiraki <dispirakieleftheria@gmail.com>
2023-03-26 20:00:00 +0100

    added manager's profile generator code

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const id = []
+const teamMembers = []
+
+const appMenu = () => {
+    function createManager(){
+        console.log("Build your team!");
+        inquirer.prompt([
+            {
+            type: "input",
+            name: "managerName",
+            message: "What's the manager's name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What's the manager's id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please write the maanger's email address!"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                messageL: "What's the manager's office number?"
+            }
+        ]).then(answers=>{
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        id.push(answers.managerId);
+        })
+    }
+    createManager();
+}
+
+appMenu();
